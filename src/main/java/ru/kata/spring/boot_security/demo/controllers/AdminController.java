39e4ed0da2f8d71575d2ac96616d116ba7174e7e
work <stif2000.95@mail.ru>
2023-03-30 17:58:44 +0300

    0.2.10

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.service.ServiceUser;
+import ru.kata.spring.boot_security.demo.utill.UserValidator;
 
 
 import javax.validation.Valid;
@@ -16,45 +17,47 @@ import javax.validation.Valid;
 public class AdminController {
 
     private final ServiceUser serviceUser;
+    private final UserValidator userValidator;
 
-    public AdminController(ServiceUser serviceUser) {
+    public AdminController(ServiceUser serviceUser, UserValidator userValidator) {
         this.serviceUser = serviceUser;
+        this.userValidator = userValidator;
     }
 
 
     @GetMapping()
     public String snowDbUsers(Model model, Authentication authentication) {
-
         User user = (User) authentication.getPrincipal();
         model.addAttribute("userAdmin", serviceUser.findUser(user.getId()));
         model.addAttribute("users", serviceUser.userShow());
         model.addAttribute("userAdd", new User());
         return "admin/show";
     }
-    @GetMapping("/add")
-    public String newUser(@ModelAttribute("user") User user) {
-        return "admin/add";
-    }
-    @PostMapping()
+//    @GetMapping("/add")
+//    public String newUser(@ModelAttribute("user") User user) {
+//        return "admin/add";
+//    }
+    @PostMapping("/add")
     public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        userValidator.validate(user, bindingResult);
         if (bindingResult.hasErrors())
-            return "admin/add";
+            return "admin/show";
         serviceUser.add(user);
         return "redirect:/admin";
     }
-    @GetMapping("edit/{id}")
-    public String editUser(@PathVariable("id") int id, Model model) {
-        model.addAttribute(serviceUser.findUser(id));
-        return "admin/edit";
-    }
-    @PatchMapping("/{id}")
+//    @GetMapping("edit/{id}")
+//    public String editUser(@PathVariable("id") int id, Model model) {
+//        model.addAttribute(serviceUser.findUser(id));
+//        return "admin/show";
+//    }
+    @PatchMapping("edit/{id}")
     public String edit(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, @PathVariable("id") int id) {
         if (bindingResult.hasErrors())
-            return "admin/edit";
+            return "admin/show";
         serviceUser.update(id, user);
         return "redirect:/admin";
     }
-    @DeleteMapping("/{id}")
+    @DeleteMapping("delete/{id}")
     public String delete(@PathVariable("id") int id) {
         serviceUser.delete(id);
         return "redirect:/admin";
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
@@ -1,23 +1,19 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.jdbc.IncorrectResultSetColumnCountException;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.AbstractBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.DispatcherServlet;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.service.ServiceUser;
-import ru.kata.spring.boot_security.demo.service.ServiceUserImpl;
 import ru.kata.spring.boot_security.demo.utill.UserValidator;
 
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/form")
+@RequestMapping("/form/login")
 public class LoginController {
     private final ServiceUser serviceUser;
     private final UserValidator userValidator;
@@ -27,25 +23,21 @@ public class LoginController {
         this.userValidator = userValidator;
     }
 
-    @GetMapping("/login")
-    public String loginPage() {
+    @GetMapping()
+    public String loginPage(@ModelAttribute("user") User user) {
         return "form/login";
     }
-
-    @GetMapping("/registration")
-    public String registrationPage(@ModelAttribute("user") User user) {
-        return "form/registration";
-    }
-
+//    @GetMapping("/registration")
+//    public String newUser(@ModelAttribute("user") User user) {
+//        return "form/login";
+//    }
     @PostMapping("/registration")
     public String perform(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
         userValidator.validate(user, bindingResult);
         if (bindingResult.hasErrors()) {
-            return "/form/registration";
+            return "/form/login";
         }
-
         serviceUser.registration(user);
-
-        return "redirect:form/login";
+        return "redirect:/form/login";
     }
 }
